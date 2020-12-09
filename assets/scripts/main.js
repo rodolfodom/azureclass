70ba404d4856205fd92c6bb1bc79c601f925b865
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2020-12-08 19:29:19 -0600

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,37 @@
+const baseURL = ()=>{
+    let arrValue = [];
+    let parameter = document.getElementById("pokemon").value;
+    if(parameter == ""){
+        document.getElementById("error").style.display = "block";
+        setTimeout(function(){
+            document.getElementById("error").style.display = "none";
+        }, 2000);
+    }else{
+        url = "https://pokeapi.co/api/v2/pokemon/" + parameter.toLowerCase();
+    fetch(url)
+        .then((response => response.json()))
+        .then(myJson => {
+            console.log(myJson)
+            document.getElementById("pokeinfo").style.display = "flex";
+            document.getElementById("image").src = myJson.sprites.other["official-artwork"].front_default;
+            if(myJson.sprites.other["official-artwork"].front_default == null){
+                document.getElementById("image").src = myJson.sprites.front_default;
+            };
+            arrValue.push(myJson.name);
+            arrValue.push(myJson.id);
+            arrValue.push(myJson.height);
+            let info = ["nombre", "ID", "altura"];
+            for(let i = 0; i < info.length; i++){
+                document.getElementById(info[i]).innerHTML = arrValue[i];
+            }
+        })
+        .catch(function(error){
+            document.getElementById("error").style.display = "block";
+            setTimeout(function(){
+                document.getElementById("error").style.display = "none";
+            }, 2000);
+        });
+    };
+}
+
+
